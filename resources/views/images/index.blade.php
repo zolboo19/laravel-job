af82ba63f3895e6bc10295bab56c4fc96a13fd7e
zolboo19 <zolboo0906@gmail.com>
2019-11-03 09:44:57 +0800

    Using JQuery to create multiple input file field

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -5,9 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Album</div>
-                <div class="card-body">
-                    <form action="{{ route('album.store')}}" method="POST" enctype="multipart/form-data">
+
+                <div class="input-group control-group initial-add-more">
+                    <input type="file" name="image[]" class="form-control" id="image">
+                    <div class="input-group-btn">
+                        <button class="btn btn-primary btn-add-more">Нэмэх</button>
+                    </div>
+                </div>
+
+                <div class="copy" style="display: none;">
+                        <div class="input-group control-group add-more">
+                            <input type="file" name="image[]" class="form-control" id="image">
+                            <div class="input-group-btn">
+                                <button class="btn btn-danger remove">Устгах</button>
+                            </div>
+                        </div>
+                </div>
+
+
+                    {{-- <form action="{{ route('album.store')}}" method="POST" enctype="multipart/form-data">
                         @csrf
 
                         <input type="text" name="album" class="form-control" placeholder="Зургийн цомгийн нэрийг оруулна уу?">
@@ -22,10 +38,24 @@
                     <br>
                     @foreach ($images as $image)
                         <img src="{{ asset('storage/'.$image->name) }}" class="img-thumbnail">
-                    @endforeach
+                    @endforeach --}}
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+    $(document).ready(function(e){
+        $(".btn-add-more").click(function(){
+            //alert("Ok");
+            var html = $(".copy").html();
+            $(".initial-add-more").after(html);
+        })
+
+        $("body").on("click", ".remove", function(){
+            $(this).parents(".control-group").remove();
+        })
+    })
+</script>
